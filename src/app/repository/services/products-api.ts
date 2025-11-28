10b40470dc0635b480c2f24861fbd4e2de2d5487
ardome21 <ardome21@gmail.com>
2025-11-27 19:50:46 -0500

    remove unused import

--- src/app/repository/services/products-api.ts
+++ src/app/repository/services/products-api.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { ProductData } from '../types/products';
 
 @Injectable({

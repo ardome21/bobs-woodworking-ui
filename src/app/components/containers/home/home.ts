e3a44cf5501ef33b6d76348bddec70782814fa0b
ardome21 <ardome21@gmail.com>
2025-12-16 14:03:58 -0500

    remove test auth

--- src/app/components/containers/home/home.ts
+++ src/app/components/containers/home/home.ts
@@ -2,8 +2,6 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { Products } from '../../../services/products';
 import { Product } from '../../../models/products';
 import { ProductsList } from '../../presenters/products-list/products-list';
-import { BrowseProducts } from '../browse-products/browse-products';
-import { Test } from '../../../repository/services/test';
 
 @Component({
     selector: 'app-home',
@@ -18,7 +16,6 @@ export class Home implements OnInit {
 
     ngOnInit() {
         this.loadProducts();
-        // this.testAuth();
     }
 
     loadProducts() {
@@ -28,19 +25,6 @@ export class Home implements OnInit {
             },
             error: (err) => {
                 console.error('Error loading products:', err);
-            },
-        });
-    }
-
-    // For testing purposes
-    private testAPI = inject(Test);
-    testAuth() {
-        this.testAPI.testTokenChecks().subscribe({
-            next: (data) => {
-                console.log('Test API response:', data);
-            },
-            error: (err) => {
-                console.error('Error during test API call:', err);
             },
         });
     }
